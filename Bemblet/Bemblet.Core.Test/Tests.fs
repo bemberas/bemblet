9daa92017b5881f24c1adc370a15b42d691b6db9
Sebastian Reinhard <sebastian@bember.com>
2018-11-16 12:48:39 +0100

    Add some failing tests

--- Bemblet/Bemblet.Core.Test/Tests.fs
+++ Bemblet/Bemblet.Core.Test/Tests.fs
@@ -3,6 +3,33 @@
 open System
 open Xunit
 
+
+open Ast
+
+let assertParsesTo template ast =
+    Assert.Equal(ast, Parser.parse template)
+
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Empty template yields empty component list`` () =
+    assertParsesTo
+        ""
+        { components = []; }
+
+[<Fact>]
+let ``Single expression`` () =
+    assertParsesTo
+        "{{foo:string}}"
+        {   components = [
+                Expr {
+                    symbol = "foo";
+                    kind = { name = "string"; constraints = []; };
+                    description = "";
+                }
+            ];
+        }
+
+[<Fact>]
+let ``Verbatim text only`` () =
+    assertParsesTo
+        "foo bar baz"
+        { components = [ Verbatim "foo bar baz" ] }
